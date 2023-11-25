39b8d336a9c2fd3ced5f965da384021df018bdaf
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2023-11-25 11:20:02 +0900

    iconsテーブルにuser_id_idxを追加

--- webapp/sql/initdb.d/10_schema.sql
+++ webapp/sql/initdb.d/10_schema.sql
@@ -109,3 +109,4 @@ ALTER TABLE `reactions` ADD INDEX `livestream_id_idx` (`livestream_id`);
 ALTER TABLE `livestream_viewers_history` ADD INDEX `livestream_id_idx` (`livestream_id`);
 ALTER TABLE `livecomments` ADD INDEX `livestream_id_idx` (`livestream_id`);
 ALTER TABLE `livecomment_reports` ADD INDEX `livestream_id_idx` (`livestream_id`);
+ALTER TABLE `icons` ADD INDEX `user_id_idx` (`user_id`);
