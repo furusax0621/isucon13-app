ca8483ce77c958d7228cc8abb5a0f306c02063a1
yoRyuuuuu <ryugo.yoshioka@fuller.co.jp>
2023-11-25 11:05:35 +0900

    10_schema.sqlにインデックスの作成をする

--- webapp/sql/initial_index.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `reactions` ADD INDEX `livestream_id_idx` (`livestream_id`);
-ALTER TABLE `livestream_viewers_history` ADD INDEX `livestream_id_idx` (`livestream_id`);
-ALTER TABLE `livecomments` ADD INDEX `livestream_id_idx` (`livestream_id`);
-ALTER TABLE `livecomment_reports` ADD INDEX `livestream_id_idx` (`livestream_id`);
