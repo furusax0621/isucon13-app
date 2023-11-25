a02abb11b75fa6107ba10bf41bdf7689a9bd08b8
Tatsuro Furusawa <fur.sax.smallworld@gmail.com>
2023-11-25 14:51:33 +0900

    コメント削除

--- webapp/go/user_handler.go
+++ webapp/go/user_handler.go
@@ -434,17 +434,6 @@ func fillUserResponse(ctx context.Context, tx *sqlx.Tx, userModel UserModel) (Us
 		}
 		iconHash = fallbackImageHash
 	}
-	// var image []byte
-	// if err := tx.GetContext(ctx, &image, "SELECT image FROM icons WHERE user_id = ?", userModel.ID); err != nil {
-	// 	if !errors.Is(err, sql.ErrNoRows) {
-	// 		return User{}, err
-	// 	}
-	// 	image, err = os.ReadFile(fallbackImage)
-	// 	if err != nil {
-	// 		return User{}, err
-	// 	}
-	// }
-	// iconHash := sha256.Sum256(image)
 
 	user := User{
 		ID:          userModel.ID,
